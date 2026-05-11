e077680c625ac0382308d653a61bc19e33afebbf
Pragati Sarkar <sarkarpragati2004@gmail.com>
2026-05-11 12:03:48 +0530

    Time: 1 ms (92.71%), Space: 19.2 MB (53.72%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,9 +1,5 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
-        
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+      
         s = str(x)
         return s == s[::-1]
